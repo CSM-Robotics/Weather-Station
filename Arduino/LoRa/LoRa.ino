1b91151d4b16843363a53e0ff07d4c2dddc0844a
Kyle Neil <kyle@johnneil.net>
2019-08-22 12:25:06 -0700

    notes on how to read everything

--- Arduino/LoRa/LoRa.ino
+++ Arduino/LoRa/LoRa.ino
@@ -1,9 +1,22 @@
-// blank for now
+/*
+
+packet structure:
+	[ temp float ] [ hum float ] [ pressure float ] [ tVOC int ] [ CO2 int ] [ Geiger int ] [ battery level float ]
+
+attach battery level to analog pins in order to detect when running out of power
+
+*/
 
 void setup() {
-
+	// set up all devices, some may require burn-in time to function properly
+		// if device requires burn-in, don't block.
+		// sleep until data is ready.
 }
 
 void loop() {
+	// get BME data over I2C - actually don't think this needs any translation code
+	// get CCS data over I2C - will need translation, want the sensor to interrupt when data is ready
+	// get Gieger data over analog pins - will need translation, do math when ready
 
+	// send everything
 }
