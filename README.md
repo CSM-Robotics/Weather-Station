fe9a1cb9d42f96e9f71c1d3d0d957eb9bc23da9a
raspi <none@none.com>
2020-05-28 19:51:07 -0700

    The whole pipeline works!

--- README.md
+++ README.md
@@ -3,31 +3,9 @@ This is the GitHub for the CSM Weather station project.
 
 The goal for this weather station is to collect pressure/temperature/humidity/VOC/radiation measurements from an Arduino system, send it to a Raspberry Pi over LoRa, and publish that data somewhere in the cloud for other people to view.  Measurements will also be stored over time so that people can view graphs of the data.
 
-Here is the [link](https://docs.google.com/spreadsheets/d/1q3k0UVijBZFMGUKlG-Q5725P6HpbFjzFsUlXNikVg40/edit?usp=sharing) to the BOM (Bill of Materials) for everything.
+More detailed information can be found on the CSM Robotics Club "Weather-Station-Documentation" repository.
 
-## Safety information
-- The boards are assembled with leaded solder, I think.  Wash your hands after touching hardware.
+## Safety Information
+- The boards are assembled with leaded solder.  Wash your hands after touching hardware.
 - If the VOC or Radiation measurements are way off, the sensors are probably broken.  Check the news before scaring yourself.
 
-## Arduino information
-The Arduino system uses multiple sensors to collect data, all of which are listed below.  For more detailed information check the Sparkfun "Getting Started" guides for the respective sensors.
-- The [BME280](https://www.sparkfun.com/products/13676) sensor is a combination pressure/temperature/humidity sensor. (3.3v **only**!!)
-- The [CCS811](https://www.sparkfun.com/products/14193) sensor measures air quality levels. (3.3v **only**!!)
-- The [Pocket Geiger Sensor](https://www.sparkfun.com/products/14209) measures beta and gamma radiation! (3.3v /  5v compatible)
-
-The CCS811 and BME280 sensors should be connected by I2C to the Pro RF.
-
-Additionally, the Arduino itself isn't just an Uno derivative.  It's actually a [Sparkfun Pro RF](https://www.sparkfun.com/products/14916), which is **only** 3.3v compatible and has a couple more features.
-The weather station also has a [Sunny Buddy](https://www.sparkfun.com/products/12885) for managing the solar panel and LiPo battery.
-
-## Raspberry Pi information
-The Raspberry Pi 4 uses an [RFM95W](https://www.adafruit.com/product/3072) in order to recieve data from the Arduino over LoRa.  It will then send data over WiFi to some kind of cloud server.
-
-## Cloud information
-Not a lot yet...
-
-## Code Rules
-- If you have previous C/C++ experience, please don't write bad code and ask me to look at it.
-- If you don't have previous C/C++ experience, feel free to ask questions about what the above phrase means.
-- If you want to add code to this repository, you must have a way to prove to me it works before I'll look at it.  (Not asking for bug-free code, but please run it >1 time(s))
-- Once your code works, either create a Pull Request or send the .ino files to me via email or something.
--- RasPi/LoRa.py
+++ RasPi/LoRa.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python3
 
-# RasPi config options needed before this script will run:
-
-# sudo apt-get update && sudo apt-get upgrade
-# sudo pip3 install --upgrade setuptools
-# add the line 'dtoverlay=spi1-3cs' to the bottom of /boot/config.txt
-# pip3 install RPI.GPIO
-# pip3 install adafruit-blinka
-# sudo pip3 install adafruit-circuitpython-rfm9x
-
-# Vin -> 3.3v
-# GND -> GND
-# RFM G0 -> GPIO #5
-# RFM RST -> GPIO #25
-# RFM CLK -> SCK
-# RFM MISO -> MISO
-# RFM MOSI -> MOSI
-# RFM CS -> CE1
-
 import busio
 import board
 import adafruit_rfm9x
@@ -36,87 +18,123 @@ import datetime # for timestamps
 import logging # for writing to a log file for debugging
 import argparse
 
+import json # for packing object data
+import requests # for talking to the AWS server
+
 packetlen = 36
-packettime = 2.0 / 60.0 # time in between packets in minutes
+packettime = 2.0 # time in between packets in minutes
 
 packettotal = 0 # packets understood
 
-center_freq = 915.0
+center_freq = 915.0 # in MHz
 
-logname = './logfile_LoRa.txt'
+def packed_to_tuple(packed_data):
+    '''converts a C++-style struct coming from the Arduino to a Python tuple for easier data processing.'''
+    dataformat = namedtuple('dataformat', 'NodeID tempC pressPa hum CO2 tVOC count packetcount deviceinfo')
+    packetformat = '<IfffffHxxII' # this is the format of a packet written in a format that struct can understand.
+    return dataformat._make(struct.unpack(packetformat, packed_data))
 
-def signal_handler(sig, frame):
-    '''traps cntl-c to print out the number of received and understood packets'''
-    
-    global packettotal
-    logging.info('caught cntl-c, exiting now.')
-    print('\ngot %d packets.' % (packettotal)) 
-    sys.exit(0)
+def tuple_to_json(tuple_data):
+    '''turns a Python tuple produced by packed_to_tuple() to a JSON object for submission to AWS.'''
+    ret_dict = {}
 
-def printpacket(pack):
-    '''takes in a packet from the LoRa hardware and parses it, returning a formatted tuple with a variety of parameters'''
+    #ret_dict['date'] = str(datetime.datetime.now())
+    ret_dict['temp'] = tuple_data.tempC
+    ret_dict['pressPa'] = tuple_data.pressPa
+    ret_dict['hum'] = tuple_data.hum
+    ret_dict['count'] = tuple_data.count / packettime # this is really the counts per minute, but the JSON that Tim's AWS server accepts calls it the count.
+    ret_dict['packetNum'] = tuple_data.packetcount
+    ret_dict['deviceInfo'] = tuple_data.deviceinfo
+    ret_dict['tvocpp'] = tuple_data.tVOC # the JSON item for this is misspelled.
+    ret_dict['co2ppm'] = tuple_data.CO2
+    return ret_dict
+
+def parsepacket(pack):
+    '''recieves a raw packet from the LoRa hardware and returns a dict containing (almost) the same data, or None if the packet was corrupted.'''
     if pack is None:
-        return False
+        return None
 
     # error checking to make sure that the packet is well-formed
     if len(pack) != packetlen:
         print('WARNING - packet is not of proper length, dropping it.')
         
-        return False
+        return None
     
-    dataformat = namedtuple('dataformat', 'NodeID tempC pressPa hum CO2 tVOC count packetcount deviceinfo')
-	
-    packetformat = '<IfffffHxxII' # this is the format of a packet written in a format that struct can understand.
-	
-    data = dataformat._make(struct.unpack(packetformat, pack))
+    data = packed_to_tuple(pack)
     
-    global packettotal
     cpm = data.count / packettime
-    print(datetime.datetime.now().strftime('%m/%d/%y %I:%M:%S %p') + '    ', end='')
-    print('host packet %d (client packet %d):    %4.2f C, %4.2f Pa, %4.2f percent hum, %4.2f ppm CO2, %4.2f ppb tVOC, %4.2f cpm' % (packettotal, data.packetcount, data.tempC, data.pressPa, data.hum, data.CO2, data.tVOC, cpm))
+    
+    return tuple_to_json(data)
 
-    return True
+def printpacket(data):
+    '''prints out a packet with the relevant information.  Not required for operation, but really useful for debugging.'''
+    global packettotal
+    print(datetime.datetime.now().strftime('%m/%d/%y %I:%M:%S %p') + '    ', end='')
+    temp_f = (9.0 / 5.0) * data['temp'] + 32.0
+    print('host packet %d (client packet %d):    %4.2f C (%4.2f F), %4.2f Pa, %4.2f percent hum, %4.2f ppm CO2, %4.2f ppb tVOC, %4.2f cpm' % (packettotal, data['packetNum'], data['temp'], temp_f, data['pressPa'], data['hum'], data['co2ppm'], data['tvocpp'], data['count']))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='a LoRa listening script.')
-    parser.add_argument('--logpath', type=str, default='.', help='path to file used to log errors and dropped packets')
+    parser.add_argument('--logpath', type=str, default='/var/log/', help='path to file used to log errors and dropped packets')
     args = parser.parse_args()
     
+    logname = 'LoRa.log' # important errors and events get recorded in the log for future data processing
+    
     print('starting LoRa script...')
-    logging.basicConfig(filename=args.logpath + '/logfile_LoRa.txt', format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%y %I:%M:%S %p', level=logging.INFO)
+    logging.basicConfig(filename=args.logpath + logname, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%y %I:%M:%S %p', level=logging.INFO)
     logging.info('starting LoRa script...')
-    signal.signal(signal.SIGINT, signal_handler)
 
     CS = DigitalInOut(board.CE1)
     RESET = DigitalInOut(board.D25)
     spi = busio.SPI(board.SCK_1, MOSI=board.MOSI_1, MISO=board.MISO_1) # using the second SPI bus, follow /boot/config.txt instructions above
-    
+
     try:
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, center_freq) # use 915MHz since we're in the US and it isn't legal to broadcast on other frequencies
         
         rfm9x.enable_crc = True # enable checksums to protect data against accidental tampering
         # a CRC is not an encryption algorithm.
-        
-        print('configured radio for %.1f MHz, %d dBm tx power' % (center_freq, rfm9x.tx_power))
-
-        num_dropped_packets = 0
-        measuring_dropped_packets = False
-
-        while True:
-            success = printpacket(rfm9x.receive(timeout = (packettime * 60) + 1))
-            if not success:
-                if not measuring_dropped_packets:
-                    measuring_dropped_packets = True
-                    logging.warning('detected a dropped packet, checking for more...')
-                num_dropped_packets += 1
-            else:
-                packettotal += 1
-                if measuring_dropped_packets:
-                    logging.warning('estimating a total of ' + str(num_dropped_packets) + ' dropped packets.')
-                    measuring_dropped_packets = False
-                    num_dropped_packets = 0
-
+    
     except RuntimeError as error:
         logging.critical('failed to initialize the LoRa module!')
         print('RFM9X initialization error: ', error)
+        sys.exit(1)
+
+    print('configured radio for %.1f MHz, %d dBm tx power' % (center_freq, rfm9x.tx_power))
+    
+    auth_addr = 'https://api.csmrobotics.club/login'
+    add_data_addr = 'https://api.csmrobotics.club/api/wss/addPackage'
+    
+    json_pass_file_path = '../../Weather-Station-Sec/AWS_info.json' # convert the file to a Python object, then to a JSON-encoded string.
+    with open(json_pass_file_path, 'r') as jpf:
+        data = json.load(jpf)
+    
+    auth = requests.post(auth_addr, data = json.dumps(data))
+    if (auth.status_code != 200):
+        print('While making a login POST request, the AWS server gave an HTTP response code of %d' % auth.status_code)
+        logging.critical('While making a login POST request, the AWS server gave an HTTP response code of %d' % auth.status_code)
+        sys.exit(1)
+    short_auth_header = {'Authorization': auth.headers['Authorization']}
+    
+    num_dropped_packets = 0
+    measuring_dropped_packets = False # instead of recording every time we get a dropped packet, we record when we start and stop noticing dropped packets and extrapolate as to the number of packets we didn't see.
+    # this method saves a lot of storage space, which matters when using SD cards for a while.
+    
+    while True:
+        data = parsepacket(rfm9x.receive(timeout = (packettime * 60) + 1)) # add a second to account for random packet delays.
+        if data is None:
+            if not measuring_dropped_packets:
+                measuring_dropped_packets = True
+                logging.warning('detected a dropped packet, checking for more...')
+            num_dropped_packets += 1
+        else:
+            printpacket(data)
+            packettotal += 1
+            if measuring_dropped_packets:
+                logging.warning('estimating a total of ' + str(num_dropped_packets) + ' dropped packets.')
+                measuring_dropped_packets = False
+                num_dropped_packets = 0
+            add_data = requests.post(add_data_addr, headers = short_auth_header, json = data)
+            if (add_data.status_code != 200):
+                print('While making a data POST request, the AWS server gave an HTTP response code of %d' % add_data.status_code)
+                logging.critical('While making a data POST request, the AWS server gave an HTTP response code of %d' % add_data.status_code)
 
